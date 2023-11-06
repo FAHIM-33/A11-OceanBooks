b133259e12d22858f4751ebfac0991deb576e065
FAHIM-33 <shahriyerfahim2012@gmail.com>
2023-11-06 06:38:33 +0600

    Firebase Configured

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, updateProfile } from "firebase/auth";
 import pt from 'prop-types'
+import auth from "../Config/firebase.config";
 
 export const AuthContext = createContext()
 
@@ -11,12 +12,12 @@ const AuthProvider = ({ children }) => {
 
 
     const createUser = (email, password) => {
-        
+
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
     const login = (email, password) => {
-        
+
         return signInWithEmailAndPassword(auth, email, password)
     }
 
@@ -28,6 +29,23 @@ const AuthProvider = ({ children }) => {
         return signInWithPopup(auth, googleProvider)
     }
 
+    const updateNameImg = (name, url) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: url
+        })
+    }
+
+
+    useEffect(() => {
+        let unSubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user)
+            setLoading(false)
+        })
+        return () => unSubscribe()
+    }, [])
+
+
     const data = {
         user,
         loading,
@@ -35,6 +53,7 @@ const AuthProvider = ({ children }) => {
         login,
         logOut,
         googleLogin,
+        updateNameImg
     }
     return (
         <AuthContext.Provider value={data}>
@@ -42,5 +61,7 @@ const AuthProvider = ({ children }) => {
         </AuthContext.Provider>
     );
 };
-
+AuthProvider.propTypes = {
+    children: pt.node,
+}
 export default AuthProvider;
