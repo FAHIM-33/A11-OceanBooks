4e22996fdaf9da299ac1f83f47d87d0a6d4c1bf7
FAHIM-33 <shahriyerfahim2012@gmail.com>
2023-11-06 06:26:43 +0600

    Added AuthProvider

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,9 +1,45 @@
+import { createContext, useEffect, useState } from "react";
+import { GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, updateProfile } from "firebase/auth";
+import pt from 'prop-types'
 
-const AuthProvider = () => {
+export const AuthContext = createContext()
+
+const AuthProvider = ({ children }) => {
+    let googleProvider = new GoogleAuthProvider();
+    let [user, setUser] = useState({})
+    let [loading, setLoading] = useState(true)
+
+
+    const createUser = (email, password) => {
+        
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const login = (email, password) => {
+        
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const logOut = () => {
+        return signOut(auth)
+    }
+
+    const googleLogin = () => {
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const data = {
+        user,
+        loading,
+        createUser,
+        login,
+        logOut,
+        googleLogin,
+    }
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={data}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
